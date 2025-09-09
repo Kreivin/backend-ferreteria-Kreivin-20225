c63c63cff5efcbe4ead72217be405fded5772561
Creivin <estudiantekreivin@gmail.com>
2025-09-09 10:50:32 -0600

    cambios

--- rsc/controllers/detalles_compras.controllers.js
+++ rsc/controllers/detalles_compras.controllers.js
@@ -1,8 +1,8 @@
 import { pool } from '../../db_conecction.js';
-// Obtener todas las clientes
-export const obtenerClientes = async (req, res) => {
+// Obtener todas las detalles_compras
+export const obtenerdetalles_compras = async (req, res) => {
     try {
-        const [result] = await pool.query('SELECT * FROM clientes');
+        const [result] = await pool.query('SELECT * FROM detalles_compras');
         res.json(result);
     } catch (error) {
         return res.status(500).json({
@@ -14,13 +14,13 @@ export const obtenerClientes = async (req, res) => {
 
 
 // Obtener una clientes por su ID
-export const obtenerCliente = async (req, res) => {
+export const obtenerdetalles_compra = async (req, res) => {
 try {
-    const id_cliente = req.params.id_categoria;
-const [result] = await pool.query('SELECT * FROM clientes WHERE id_clientes = ?', [id_cliente]);
+    const id_detalles_compra = req.params.id_categoria;
+const [result] = await pool.query('SELECT * FROM detalles_compras WHERE id_detalle_compra = ?', [id_detalles_compra]);
 if (result.length <= 0) {
 return res.status(404).json({
-mensaje: `Error al leer los datos. ID ${id_cliente} no encontrado.`
+mensaje: `Error al leer los datos. ID ${id_detalles_compra} no encontrado.`
 });
 }
 res.json(result[0]);
@@ -30,4 +30,3 @@ mensaje: 'Ha ocurrido un error al leer los datos de las categorias.'
 });
 }
 };
-
--- rsc/routes/categorias.routes.js
+++ rsc/routes/categorias.routes.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
-import { obtenerCategorias } from '../controllers/categorias.controllers.js';
+import { obtenerCategorias, obtenerCategoria } from '../controllers/categorias.controllers.js';
 
 const router = Router();
 
 // Ruta para obtener todas las categorías
 router.get('/categorias', obtenerCategorias);
+
+// ruta para obtener una categoría por su ID
+router.get('/categoria/:id', obtenerCategoria);
+
+// Ruta para registrar una nueva categoría
+router.post('/categoria', registrarCategoria);
+
 export default router;
